14133730637381009271003be19b306ac8d9a588
RDaniloMM <rdmoronm@unjbg.edu.pe>
2025-09-09 15:37:51 -0500

    feat: agregar AuthButton y mejorar la interfaz de usuario en el layout protegido, actualizar rutas

--- app/admin/(protected)/layout.tsx
+++ app/admin/(protected)/layout.tsx
@@ -1,4 +1,4 @@
-//import { AuthButton } from "@/components/auth-button";
+import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 
 export default function ProtectedLayout({
@@ -10,8 +10,8 @@ export default function ProtectedLayout({
     <main className='min-h-screen flex flex-col items-center'>
       <div className='flex-1 w-full flex flex-col gap-20 items-center'>
         <nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
-          <div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
-            {/* <AuthButton /> */}
+          <div className='w-full max-w-5xl flex justify-end p-3 px-5 text-sm'>
+            <AuthButton />
           </div>
         </nav>
         <div className='flex-1 flex flex-col gap-20 max-w-5xl p-5'>
