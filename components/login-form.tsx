0e9225da367ce95c10f7b07805ba3682b92a89c8
RDaniloMM <rdmoronm@unjbg.edu.pe>
2025-09-09 22:08:19 -0500

    Login centrado

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { createClient } from "@/lib/supabase/client";
 import { Button } from "@/components/ui/button";
@@ -42,13 +42,15 @@ export function LoginForm({
         password,
       });
       if (error) throw error;
-      
+
       // Redirige a la página protegida tras un login exitoso.
       router.push("/admin/dashboard"); // Cambia "/protected" por tu ruta de dashboard si es diferente
       router.refresh(); // Refresca la página para actualizar el estado de la sesión
     } catch (error: unknown) {
       setError(
-        error instanceof Error ? "Usuario o contraseña incorrectos." : "Ocurrió un error inesperado."
+        error instanceof Error
+          ? "Usuario o contraseña incorrectos."
+          : "Ocurrió un error inesperado."
       );
     } finally {
       setIsLoading(false);
@@ -56,31 +58,41 @@ export function LoginForm({
   };
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div
+      className={cn("flex flex-col gap-6", className)}
+      {...props}
+    >
       <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Iniciar Sesión</CardTitle>
+        <CardHeader className='text-center'>
+          <Image
+            className='item-center mx-auto'
+            src='/logo.png'
+            width={150}
+            height={150}
+            alt='Logo Dental Company'
+          />
+          <CardTitle className='text-2xl'>Iniciar Sesión</CardTitle>
           <CardDescription>
             Ingresa tu usuario y contraseña para acceder al sistema.
           </CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleLogin}>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="username">Usuario</Label>
+            <div className='flex flex-col gap-6'>
+              <div className='grid gap-2'>
+                <Label htmlFor='username'>Usuario</Label>
                 <Input
-                  id="username"
-                  type="text"
-                  placeholder="ej: admin1"
+                  id='username'
+                  type='text'
+                  placeholder='ej: admin1'
                   required
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Contraseña</Label>
+              <div className='grid gap-2'>
+                <div className='flex items-center'>
+                  <Label htmlFor='password'>Contraseña</Label>
                   {/* Puedes descomentar esto si implementas recuperación de contraseña */}
                   {/* <Link
                     href="/auth/forgot-password"
@@ -90,23 +102,27 @@ export function LoginForm({
                   </Link> */}
                 </div>
                 <Input
-                  id="password"
-                  type="password"
+                  id='password'
+                  type='password'
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              {error && <p className="text-sm text-red-500">{error}</p>}
-              <Button type="submit" className="w-full" disabled={isLoading}>
+              {error && <p className='text-sm text-red-500'>{error}</p>}
+              <Button
+                type='submit'
+                className='w-full'
+                disabled={isLoading}
+              >
                 {isLoading ? "Iniciando sesión..." : "Iniciar Sesión"}
               </Button>
             </div>
-            <div className="mt-4 text-center text-sm">
+            <div className='mt-4 text-center text-sm'>
               ¿No tienes una cuenta?{" "}
               <Link
-                href="/admin/sign-up"
-                className="underline underline-offset-4"
+                href='/admin/sign-up'
+                className='underline underline-offset-4'
               >
                 Regístrate
               </Link>
