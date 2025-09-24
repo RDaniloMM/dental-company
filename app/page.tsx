499d0128eb3d7157f0906615adcff5e832acacaf
RDaniloMM <rdmoronm@unjbg.edu.pe>
2025-09-24 11:29:44 -0500

    fix: actualizar estilos en el encabezado y modificacion de base de datos v3.0

--- app/page.tsx
+++ app/page.tsx
@@ -226,7 +226,7 @@ export default function LandingPage() {
           className='py-20 bg-blue-50'
         >
           <div className='container mx-auto px-6 text-center'>
-            <h2 className='text-3xl font-bold mb-12'>
+            <h2 className='text-3xl font-bold mb-12 text-black'>
               Conoce a Nuestros Especialistas
             </h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
