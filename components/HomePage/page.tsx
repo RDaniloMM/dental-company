fef9d1e4b6b3b32c8f7066942fb3aaccf5367a8f
SGCOx119009 <tapion1123@gmail.com>
2025-11-24 05:20:39 -0500

    fix: dashboard - page.tsx

--- components/HomePage/page.tsx
+++ components/HomePage/page.tsx
@@ -1,11 +1,6 @@
-import { createClient } from "@/lib/supabase/server";
 import CalendarHome from "@/components/calendar/CalendarHome";
 
 export default async function HomePage() {
-  const supabase = await createClient();
-  const { data: patients } = await supabase
-    .from("pacientes")
-    .select("id, nombres, apellidos, numero_historia");
   return (
     <>
       <main className="h-full">
@@ -14,6 +9,20 @@ export default async function HomePage() {
             <h3 className="font-semibold text-base sm:text-lg">Resumen</h3>
             <p className="text-sm sm:text-base text-muted-foreground mt-1">
               Métricas y estado general.
+            </p>
+          </div>
+
+          <div className="rounded-lg border border-border bg-card p-4 sm:p-6 text-card-foreground">
+            <h3 className="font-semibold text-base sm:text-lg">Metrica 1</h3>
+            <p className="text-sm sm:text-base text-muted-foreground mt-1">
+              Métrica detallada.
+            </p>
+          </div>
+
+          <div className="rounded-lg border border-border bg-card p-4 sm:p-6 text-card-foreground">
+            <h3 className="font-semibold text-base sm:text-lg">Metrica 2</h3>
+            <p className="text-sm sm:text-base text-muted-foreground mt-1">
+              Métrica detallada.
             </p>
           </div>
           
